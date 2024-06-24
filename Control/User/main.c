bd9e9420ef9fee0f0c7f0cb99a6e1c81a14fee26
SHR-sky <1779558876@qq.com>
2024-06-24 21:26:00 +0800

    温湿度传感器以及串口通信

---
+++ Control/User/main.c
@@ -0,0 +1,32 @@
+#include "stm32f4xx.h"
+#include "TIM.h"
+#include "GPIO.h"
+#include "DHT11.h"
+#include "Serial.h"
+
+uint8_t temperature, dec;
+
+
+int main(void)
+{
+    // 初始化
+    Data_Out_Init();
+    Data_In_Init();
+    tDHT11_start();
+    
+    Serial_Init();
+	
+    while(1);
+}
+
+
+
+void TIM2_IRQHandler(void)
+{
+	tDHT11_rec_data(&temperature, &dec);
+    Serial_Printf("%d",temperature);
+    Serial_Printf("%d",dec);
+}
+
+
+
